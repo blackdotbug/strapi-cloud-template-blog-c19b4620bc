0256a72f0b12cd1262bc175cd1c5e50256ae4621
Heather Bree <me@justheatherbree.com>
2025-07-01 13:39:38 -0700

    parse strapi content

--- svelte/src/routes/+page.svelte
+++ svelte/src/routes/+page.svelte
@@ -3,5 +3,30 @@
 	let { data }: PageProps = $props();
 </script>
 
-{@debug data}
 <h1>Just testing</h1>
+{#each data.data as card}
+	<div>
+		{#if card.display_title}
+			<h2>{card.title}</h2>
+		{/if}
+		{#each card.card_content as content}
+			{#if content.infographic}
+				{#each content.infographic.content as info}
+					{#if info.number}
+						<div>{info.number} {info.units}</div>						
+					{/if}
+					{#if info.text}
+						<div>{info.text}</div>
+					{/if}
+					{#if info.svg}
+						{@html info.svg}
+					{/if}
+				{/each}
+			{:else if content.chart}
+				{#if content.chart.display_title}
+					<h3>{content.chart.title}</h3>
+				{/if}
+			{/if}
+		{/each}
+	</div>
+{/each}
--- svelte/src/routes/+page.ts
+++ svelte/src/routes/+page.ts
@@ -3,13 +3,12 @@ export const prerender = true;
 import type { PageLoad } from './$types';
 
 export const load: PageLoad = async ({ fetch }) => {
-  //     populate[chart][populate][0]=data_source
-  const base_strapi = "https://worthy-sunrise-ddd60f1bea.strapiapp.com/";
-	const res = await fetch(`${base_strapi}api/articles?populate=*`, {
+  const base_strapi = "https://timely-unity-15efc06ae3.strapiapp.com/";
+	const res = await fetch(`${base_strapi}api/cards?populate[card_content][on][card-content.card-infographic][populate][infographic][populate][0]=content&populate[card_content][on][card-content.card-chart][populate][chart][populate][0]=datasource`, {
     headers: {
-      Authorization: `Bearer bd8963a0a9378127ebbaf0c2609568b44e8ece53586847df4476edb04d7d989b027c26d4174a2842dc9f834e02a6e23321e231e79eac0d2dfdb5f6a957bc6f9033b51dd6d9f986f1fefff40764e19ca6f511004cc479a38f88417a6d7f61b4e7a0d9fe1fe9a1845cac532a561e910c4677fac870e566463091e8fe73c53c1eb0`
+      Authorization: `Bearer 43b622fc00d8ad3f6d2a2c3f1c5920e4b2ea92351079298d8bede0b094b65b1f99d758aeeb7f51267594b1b9af47d5beef70e566ce2db18c9e19f9a5ab189a695ebed9589a0541717cdd4768ba92525072bfddaeeb6ee01ab8f384552b5d9bb67309b4ce4f8101122854b6ce9c5a0119938050a85719d75a9cde05d394302d8c`
     }});
 	const res_json = await res.json();
 
-	return { result: res_json };
+	return res_json;
 };
